2d6e90cc4bcdd3060710c468c6eb7fdb29789974
xiaobai <xiaobai@xiaobai.com>
2019-08-17 15:38:10 +0800

    add  demo

---
+++ config.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# coding:utf-8
+
+TOKEN = '934856189:AAGry-EqwdOxGh7Tax5hbNqy-zuss7wsQfg'
+
---
+++ telegram_bot.py
@@ -0,0 +1,44 @@
+import requests
+import telebot
+
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    print(message.from_user.username + ':' + message.text)
+    bot.send_message(message.chat.id, '大家好，我是机器人')
+
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    print(message.from_user.username + ':' + message.text)
+    bot.send_message(reply_to_message_id=message.message_id, chat_id=message.chat.id, text='有什么可以帮您')
+
+
+@bot.message_handler()
+def echo(message):
+    print(message.from_user.username + ':' + message.text)
+    text = get_tuling_response(message.from_user.username, message.text)
+    text = text.replace('{br}', '\n')
+
+    bot.reply_to(message, text)
+
+
+def get_tuling_response(userid, _info):
+    # print(_info)
+
+    api_url = 'http://api.qingyunke.com/api.php?key=free&appid=0&msg=' + _info
+
+    # 发送数据到执行网址
+    res = requests.post(api_url, {}).json()
+    # print(res, type(res))
+    # 给用户返回数据
+    # print(res['content'])
+    return res['content']
+
+
+if __name__ == '__main__':
+    bot.polling()
